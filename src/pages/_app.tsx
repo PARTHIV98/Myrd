5efa3e66cf861426ab9784508b27442bdf67f212
Parthiv Patel <parthivpatel14398@gmail.com>
2022-06-08 01:13:19 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import 'faust.config';
 import { FaustProvider } from '@faustjs/next';
 import 'normalize.css/normalize.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'scss/main.scss';
 import { client } from 'client';
 import type { AppProps } from 'next/app';
@@ -9,6 +9,12 @@ import { useRouter } from 'next/router'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
+  const refreshData = () => {
+    router.replace(router.asPath);
+  }
+  useEffect(() => {
+     refreshData()
+  }, [])
   return (
     <>
       <FaustProvider client={client} pageProps={pageProps}>
