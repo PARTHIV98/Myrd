abcb2d28783298b3e3c80f9cb3d60774a826e4f8
Parthiv Patel <parthivpatel14398@gmail.com>
2022-06-10 01:10:57 +0530

    Delete src directory

--- src/src/components/SiteLayout.tsx
+++
@@ -1,5 +0,0 @@
-const SiteLayout = ({ children }) => <div>{/* ... */}</div>
-
-export const getLayout = page => <SiteLayout><div className="blockLevel">{page}</div></SiteLayout>
-
-export default SiteLayout
